0099e30b3ed3329967f5dda5a6c9f4ce418de436
Daniel From Kjærgaard <KongzCringe@gmail.com>
2023-03-14 15:15:43 +0100

    Basic slot maskine virker

--- Assets/Scripts/Daniel/Icon.cs
+++ Assets/Scripts/Daniel/Icon.cs
@@ -25,6 +25,12 @@ public class Icon : MonoBehaviour
     [SerializeField] private int baseMultiplier;
     [SerializeField] private int dropChance;
 
+    public Vector2 location;
+    
+    private bool movedDown = false;
+
+    private float timer = 0;
+    
     public int GetMultiplier()
     {
         return baseMultiplier;
@@ -35,8 +41,38 @@ public class Icon : MonoBehaviour
         return dropChance;
     }
     
+    public bool GetMovedDown()
+    {
+        return movedDown;
+    }
+    
     public Icons GetIconType()
     {
         return iconType;
+    }
+
+    public void UpdateMovedDown()
+    {
+        movedDown = transform.position == new Vector3(location.x, location.y, transform.position.z);
+    }
+
+    IEnumerator DropDown()
+    {
+        if (movedDown) yield break;
+
+        var pos = transform.position;
+        
+        var startPos = pos;
+        var targetPos = new Vector3(location.x, location.y, pos.z);
+
+        yield return new WaitForEndOfFrame();
+        while (transform.position != targetPos)
+        {
+            transform.position = Vector3.Lerp(startPos, targetPos, timer);
+            timer += Time.deltaTime * 2;
+            yield return null;
+        }
+
+        movedDown = true;
     }
 }
--- Assets/Scripts/Daniel/SlotManager.cs
+++ Assets/Scripts/Daniel/SlotManager.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor;
 using UnityEngine;
 using Random = System.Random;
 
 public class SlotManager : MonoBehaviour
 {
+    //Hvis ikonet allerede er spawnet, så skal den ikke spanwne det igen
+    //
+    
     [SerializeField] Icon[] icons;
     [SerializeField] Icon emptyIcon;
     
@@ -29,7 +33,11 @@ public class SlotManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.K)) RollSlot(true);
 
-        if (Input.GetKeyDown(KeyCode.Space)) GetAllIcons();
+        if (Input.GetKeyDown(KeyCode.Space)) StartCoroutine(GetAllIcons());
+
+        if (Input.GetKeyDown(KeyCode.L)) RollSlot(false);
+
+        if (Input.GetKeyDown(KeyCode.J)) MoveIconsDown();
     }
 
     private void RollSlot(bool reRoll)
@@ -43,8 +51,12 @@ public class SlotManager : MonoBehaviour
                     slots[i, j] = GetRandomIcon();
                     continue;
                 }
-                
-                if (slots[i, j].GetIconType() == Icon.Icons.Empty) slots[i, j] = GetRandomIcon();
+
+                if (slots[i, j].GetIconType() == emptyIcon.GetIconType())
+                {
+                    slots[i, j] = GetRandomIcon();
+                    print("Virkede");
+                }
             }
         }
         
@@ -64,17 +76,18 @@ public class SlotManager : MonoBehaviour
         {
             for (int j = 0; j < slots.GetLength(1); j++)
             {
-                print(slots[i,j].name);
                 var spawnIcon = Instantiate(slots[i,j].gameObject, 
                     new Vector3(i - slots.GetLength(0) / 2, j - slots.GetLength(1) / 2, 0), 
                     Quaternion.identity);
+
+                spawnIcon.GetComponent<Icon>().location = new Vector2(i, j);
 
                 spawnedIcons.Add(spawnIcon);
             }
         }
     }
 
-    private void GetAllIcons()
+    private IEnumerator GetAllIcons()
     {
         var types = new List<Icon>();
         
@@ -97,32 +110,66 @@ public class SlotManager : MonoBehaviour
             */
 
             if (amount < 8) continue;
-    
-            foreach (var icon in spawnedIcons.Where(
+
+            foreach (var spawnedIcon in spawnedIcons.Where(
                          icon => icon.GetComponent<Icon>().GetIconType() == type.GetIconType()).ToList())
             {
-                spawnedIcons.Remove(icon);
-                Destroy(icon);
+                spawnedIcons.Remove(spawnedIcon);
+                var location = spawnedIcon.GetComponent<Icon>().location;
+                slots[(int) location.x, (int) location.y] = emptyIcon;
+                Destroy(spawnedIcon);
             }
 
-            for (int i = 0; i < slots.GetLength(0); i++)
-            {
-                for (int j = 0; i < slots.GetLength(1); i++)
-                {
-                    if (slots[i, j].GetIconType() == type.GetIconType()) slots[i, j] = emptyIcon;
-                }
-            }
+            DrawScreen();
         }
 
-        RollSlot(false);
+        yield return null;
     }
-    
+
+    private void MoveIconsDown()
+    {
+        foreach (var spawnedIcon in spawnedIcons)
+        {
+            var icon = spawnedIcon.GetComponent<Icon>();
+            print("wtf");
+
+            if (icon.GetIconType() != Icon.Icons.Empty) continue;
+                
+            print("Fundet");
+
+            var location = icon.location;
+            
+            while (IsThereEmpty((int) location.x)) 
+                MoveRowDown((int) location.x, (int) location.y);
+        }
+        
+        DrawScreen();
+    }
+
+    private void MoveRowDown(int row, int startPoint)
+    {
+        for (int i = startPoint; i < slots.GetLength(1) - 1; i++)
+        {
+            slots[row, i] = slots[row, i + 1];
+            slots[row, i].location = new Vector2(row, i);
+            slots[row, i + 1] = emptyIcon;
+        }
+    }
+
+    private bool IsThereEmpty(int row)
+    {
+        for (int i = 0; i < slots.GetLength(1) - 1; i++)
+        {
+            if (slots[row, i].GetIconType() == Icon.Icons.Empty &&
+                slots[row, i + 1].GetIconType() != Icon.Icons.Empty) return true;
+        }
+        return false;
+    }
+
     private Icon GetRandomIcon()
     {
         var rndValue = rnd.Next(0, GetMaxChance() + 1);
 
-        //print("rndValue: " + rndValue + " maxChance: " + GetMaxChance() + "");
-        
         var currentChance = 0;
         var returnIcon = emptyIcon;
         
