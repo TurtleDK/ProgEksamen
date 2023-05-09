f46b0d45d907c308205691f640f2a9bd985e62b9
Darkedit <ae@haah.dk>
2023-05-09 12:11:34 +0200

    cap

--- Assets/Scripts/Daniel/SlotManager.cs
+++ Assets/Scripts/Daniel/SlotManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UIElements;
 using Random = System.Random;
